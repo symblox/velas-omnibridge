baabc8310d48150dba1d8563b7131ee42a8307f6
Symblox Dev <symblox@protonmail.com>
2020-11-26 22:11:01 +0800

    Set default language by browser

--- packages/react-app/src/contexts/LanguageContext.jsx
+++ packages/react-app/src/contexts/LanguageContext.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { IntlProvider } from 'react-intl';
 import enUs from '../language/enUs';
 import zhCn from '../language/zhCn';
@@ -9,7 +9,16 @@ export const LanguageContext = React.createContext({});
 
 export const LanguageProvider = ({ children }) => {
   const [language, setLanguage] = useState(languageOptions[0]);
-
+  useEffect(() => {
+      const browserLang = navigator.language.toLowerCase();
+      if (browserLang === "zh-cn") {
+          setLanguage(languageOptions[0]);
+        } else if (browserLang === "es-es" || browserLang === "en-us") {
+          setLanguage(languageOptions[1]);
+        } else {
+          setLanguage(languageOptions[0]);
+        }
+    });
   return (
     <LanguageContext.Provider
       value={{
